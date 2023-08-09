4d163582674ea6ae43ace74df5e9e0cf77b0acb8
Koorosh <kourosh.ashrafi@zoodfood.com>
2023-08-09 10:40:48 +0330

    move connectionString to program.cs

--- Clean-Architecture-Template/src/API/Program.cs
+++ Clean-Architecture-Template/src/API/Program.cs
@@ -1,9 +1,25 @@
 using API.Extensions.ServiceCollectionExt;
 using Infrastructure.Repositories.UnitOfWork;
+using Application.Exceptions.RuntimeExceptions;
+using Infrastructure.Persistance.EFCore;
+using Infrastructure.Caching.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddConnectionStrings(config: builder.Configuration);
+// builder.Services.AddConnectionStrings(config: builder.Configuration);
+string? mySqlConnectionString = builder.Configuration.GetConnectionString("MySql");
+string? redisConnectionString = builder.Configuration.GetConnectionString("Redis");
+
+if (mySqlConnectionString == null || redisConnectionString == null)
+{
+    throw new ConnectionStringException();
+}
+
+//EF Core
+builder.Services.AddEFCoreMySqlDbContext(mySqlConnectionString);
+//Redis
+builder.Services.AddRedis(redisConnectionString);
+
 builder.Services.AddMapperServices();
 builder.Services.AddRepositoryServices();
 builder.Services.AddBusinessServices();
