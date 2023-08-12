c173a3979d278b8139a0957b131b428fbbe696e5
Koorosh <kourosh.ashrafi@zoodfood.com>
2023-08-12 15:02:17 +0330

    snakeCase db

--- Clean-Architecture-Template/src/Infrastructure/Persistance/EFCore/EFCoreRegistration.cs
+++ Clean-Architecture-Template/src/Infrastructure/Persistance/EFCore/EFCoreRegistration.cs
@@ -1,17 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Infrastructure.Persistance.EFCore;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Infrastructure.Persistance.EFCore
+public static class EFCoreRegistration
 {
-    public static class EFCoreRegistration
+    public static void AddEFCoreMySqlDbContext(this IServiceCollection services, string connectionString, int timeout = 30)
     {
-        public static void AddEFCoreMySqlDbContext(this IServiceCollection services, string connectionString)
+        services.AddDbContext<SampleDbContext>(options =>
         {
-            services.AddDbContext<SampleDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-        }
+            options
+                .UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString),
+                    options =>
+                    {
+                        options.EnableRetryOnFailure();
+                        options.CommandTimeout(timeout);
+                    }
+                ).UseSnakeCaseNamingConvention();
+        });
     }
 }
